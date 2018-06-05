014be425b92308ac48c66268a71b4e468dbea969
Anusha Konatala <anu.konatala@gmail.com>
2018-06-05 22:12:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
     min_value = name_hash.values[0]
     name_hash.each do |name, hash|
-      if min_value < hash
+      if min_value > hash
         min_value = hash
       end
     end
